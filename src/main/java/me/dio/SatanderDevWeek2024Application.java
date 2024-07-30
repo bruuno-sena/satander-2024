7075ea616dfe2604e204e367433af28d7201cf19
Bruno Sena <bsena8@hotmail.com>
2024-07-29 22:55:54 -0300

    Initial commit

---
+++ src/main/java/me/dio/SatanderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatanderDevWeek2024Application.class, args);
+	}
+
+}
